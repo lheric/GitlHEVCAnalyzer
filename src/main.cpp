0e6ccfda9c90d597eda7ef646bade4a94ae9220b
lheric <lihuang55555@gmail.com>
2014-04-07 21:02:35 +0800

    clean up include

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,6 @@ static void xMessageOutput(QtMsgType type, const QMessageLogContext &context, co
 
 }
 
-#include "views/gitlcolorpicker.h"
 int main(int argc, char *argv[])
 {
     /// intall message handler
